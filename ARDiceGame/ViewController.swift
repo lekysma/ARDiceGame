025b37f54155d68e2f9f38402f1ae4aef0303904
lekysma <jeanmartinkyssama@yahoo.com>
2020-03-05 21:41:35 +0000

    code pour comment créer une surface horizontale et la visualiser. but pour la suite c'est poser une figure géométrique dessus

--- ARDiceGame/ViewController.swift
+++ ARDiceGame/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // on va pouvoir visualiser plus facilement la detection d'une surface horizontale
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
 //        // on cree une sphere et on lui donne un rayon
 //        let sphere = SCNSphere(radius: 0.14)
 //        // on cree un materiel pour ajouter details au cube
@@ -41,19 +44,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada copy.scn")!
-
-        // on cree une scene avec comme identifiant celui de notre dé
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-            
-            // on positionne le dé
-            diceNode.position = SCNVector3(0, 0.22, -0.44)
-            
-            //enfin on place le node dans la scene qui affiche la figure
-            sceneView.scene.rootNode.addChildNode(diceNode)
-        } else {
-            print("Impossible d'afficher la figure")
-        }
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada copy.scn")!
+//
+//        // on cree une scene avec comme identifiant celui de notre dé
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//
+//            // on positionne le dé
+//            diceNode.position = SCNVector3(0, 0.22, -0.44)
+//
+//            //enfin on place le node dans la scene qui affiche la figure
+//            sceneView.scene.rootNode.addChildNode(diceNode)
+//        } else {
+//            print("Impossible d'afficher la figure")
+//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -61,6 +64,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // on donne la possibilité de detecter une surface horizontale
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -71,6 +77,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    //MARK: - Delegate qui permet de detecter une surface horizontale, ID ses dimensions et y placer un objet
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            //succes
+            
+            // on cree une tuile sur laquelle on va fixer notre figure
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            // on cree un node a qui on va donner une position
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+            
+            // on transforme ce plan (qui par defaut est vertical) en plan horizontal via la formule suivante
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            // nous allons utiliser un fichier grille pour  visualiser notre surface plane
+            let planeMaterial = SCNMaterial()
+            planeMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            // on relie le material a notre surface
+            plane.materials = [planeMaterial]
+            // on relie notre surface au node
+            planeNode.geometry = plane
+            //enfin on ajoute un child node : deux ecritures dans ce delegate
+            //1. avec parametre node
+            node.addChildNode(planeNode)
+            //2.version habituelle
+            //sceneView.scene.rootNode.addChildNode(planeNode)
+            
+            print("succes!")
+            
+        } else {
+            // on sort de cette fonction
+            return
+        }
     }
 
     // MARK: - ARSCNViewDelegate
